a73578c8153163e882a3388766325fc120a2b637
Mdealba <M_dealba@yahoo.com>
2018-05-02 17:08:32 -0700

    created new function & for loop

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Word-Guess-Game
+# Psychic-Game (Correct Name of Assignment Completed)
+## About
+This is a game in which the user has 9 opportunities to guess the correct letters. If the correct letter is chosen then the score is incremented by 1 point. If the incorrect letter is chosen the score of losses is incremented by 1. For everysingle guess that is made the guesses left number decreases by 1. Once all 9 guesses have been made the game will end and you will receive an alert letting you know if you won or lost. 
+
+## Technologies Used
+- HTML 
+- Javascript
+
+## Incomplete
+Need to fix the code as I was unable to have it show my logic on the page. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,65 @@
 // 1. List out the letters that the computer has to choose from. This is done by created an array.
 
-var lettersOfTheAlphabet = ["a", "b", "c", "d",
-    "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var stringOfLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "x"];
 
 // 2. Set the initial variable value
 
-var guessesLeft = 9
 var wins = 0;
 var losses = 0;
+var guessesLeft = 9
+var userGuess;
+var comGuess;
 
 // 3. Create an array that will contain the guessed letters. 
+
 var guessedLetters = [];
 
-// var guessesLeft = null;
-var lettersGuessed = function (name) {
-    console.log(name);
+// 4. Create a function that will allow the computer to chose a letter at random and store it. 
+// 5. Create onkeyup that will contain a function that will lower case the letters, in case the user has the caps lock on or is pressing the shift key.
+
+document.onkeyup = function (event) {
+    userGuess = event.key.toLowerCase()
+    console.log(userGuess)
 }
-lettersGuessed()
-document.onkeyup = function () {
-    var userguess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    console.log(userguess);
+function computerGuess() {
+    var rando = Math.floor(Math.random() * 26)
+    comGuess = stringOfLetters[rando]
+    console.log(comGuess)
+}
 
-    var computerGuess = options[Math.floor(Math.random() * options.length)];
+// function game() {
+    computerGuess()
+    document.onkeyup = function (event) {
+        // only accept the letters of the alphabet (do an if statement)
+        // loop thru the alphabet array. if user guess === stringOfLetters[i]
+        // give a message stating that this isn't a valid choice try again. 
+        userGuess = event.key.toLowerCase()
+        console.log(userGuess)
+        if (comGuess === userGuess) {
+            wins++;
+            document.getElementById("wins").innerHTML = wins
+            guessesLeft = 9
+            document.getElementById("guessesLeft").innerHTML = guessesLeft
+            //This is for feedback to user
+            //<p id="message"></p>
+           //document.getElementById("message").innerHTML = "You're Psychic!!!!!"
 
-    console.log(computerGuess);
-
-    if (options <= (options.length - 1)) {
-        documents.querySelector("options").innerHTML = "Final Score; " + score + " out of " + options.lenght;
+        }  
+         else {
+            guessesLeft -- 
+            console.log("decrement",guessesLeft)
+            document.getElementById("guessesLeft").innerHTML = guessesLeft
+            check()
+        }
     }
 
-    // (userguess === "a", "e", "i", "o", "u") {
-    // wins++;
-}
-
-
-            // document.onkeyup = function(event) {
-            //     if (questionIndex === questions.length){
-            //         return;
-            //     }
-            // }
-
-            // var userInput = event.key.toLowerCase();
-    
-        
+    function check() {
+        if (guessesLeft === 0){
+            console.log("lose", guessesLeft)
+            losses++
+            document.getElementById("losses").innerHTML = losses
+            guessesLeft = 9
+            document.getElementById("guessesLeft").innerHTML = guessesLeft
+        }
+    }
